111385aa3a6c9d7d709ce28a776f697f240b7556
shuukeyare099 <koryaal6@gmail.com>
2020-11-04 01:25:23 -0800

    my first-commit

---
+++ src/Utilits/data.js
@@ -0,0 +1,360 @@
+import uuid from 'uuid/v4';
+export const phoneData = [
+    {
+      id: uuid(),
+      brand: 'Xiaomi',
+      img: 'Xiaomi-1.jpg',
+      name: `Redmi 7`,
+      price: 130,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Xiaomi',
+      name: `Redmi K20 Pro`,
+      img: 'Xiaomi-2.jpg',
+      price: 350,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Xiaomi',
+      name: `Mi 9t`,
+      img: 'Xiaomi-3.jpg',
+      price: 300,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Samsung',
+      img: 'sumsung-1.jpg',
+      name: `Galaxy A10`,
+      price: 130,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Samsung',
+      name: `Galaxy S10+`,
+      img: 'sumsung-5.jpg',
+      price: 120,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Samsung',
+      name: `Galaxy A70`,
+      img: 'sumsung-6.jpg',
+      price: 330,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Samsung',
+      name: `Galaxy Note9`,
+      img: 'sumsung-4.jpg',
+      price: 650,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Huawei',
+      name: `P30 Pro`,
+      img: "Huawei-1.jpg",
+      price: 760,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Huawei',
+      name: `Mate 20 Pro`,
+      img: "Huawei-2.jpg",
+      price: 580,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Huawei',
+      img: "Huawei-3.jpg",
+      name: `P20 Lite`,
+      price: 200,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Huawei',
+      name: `P30`,
+      img: "Huawei-4.jpg",
+      price: 550,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Huawei',
+      name: `Y9 Prime`,
+      img: "Huawei-5.jpg",
+      price: 220,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Apple',
+      name: `Iphone 7 Plus`,
+      img: "apple-1.jpg",
+      price: 690,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Apple',
+      name: `Iphone X`,
+      img: "apple-2.jpg",
+      price: 1000,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Apple',
+      name: `Iphone 6s`,
+      img: "apple-3.jpg",
+      price: 500,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Apple',
+      name: `Iphone 8 Plus`,
+      img: "apple-4.jpg",
+      price: 770,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Lenovo',
+      name: `Z6 Pro`,
+      img: 'HTC-3.jpg',
+      price: 400,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Lenovo',
+      name: `A6000`,
+      img: "Huawei-7.jpg",
+      price: 100,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'Lenovo',
+      name: `A7000`,
+      img: "Huawei-8.jpg",
+      price: 150,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'HTC',
+      name: `U19e`,
+      img: 'HTC-1.jpg',
+      price: 420,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    },
+    {
+      id: uuid(),
+      brand: 'HTC',
+      name: `10`,
+      img: 'HTC-2.jpg',
+      price: 300,
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eum magnam nesciunt esse nihil pariatur cupiditate fugit aut inventore minus facilis provident, nisi fuga! Dicta, saepe. Incidunt ipsum voluptatum odio',
+      stats:
+      {
+        size: '6.26 inches, 97.8 cm2 ',
+        resolution: '720 x 1520 pixels, 19:9 ratio',
+        GPU: 'Mali-G76 MP12 - EMEA',
+        CPU: 'Octa-core, 2x2.73 GHz Mongoose M4',
+        camera: '12+ 2MP rear camera',
+        battery: '1400 mAh',
+      }
+    }
+  ]
